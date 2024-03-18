1400912b4424d785b6acd6f9dd20e60f18ff845f
Duong Nguyen <duongduongpho@gmail.com>
2024-03-18 09:46:36 +0700

    CRUD API

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('student', [StudentController::class, 'index']);
+
+Route::post('student', [StudentController::class, 'upload']);
+
+Route::put('student/edit/{id}', [StudentController::class, 'edit']);
+
+Route::delete('student/edit/{id}', [StudentController::class, 'delete']);
+
+
